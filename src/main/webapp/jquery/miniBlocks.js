8a6d1efaa31b74d2b3b5be72d9b0c464d7a3be25
romanmelnyk <rrmelnyk@gmail.com>
2015-04-29 04:58:58 +0300

    Main page finished

--- src/main/webapp/jquery/miniBlocks.js
+++ src/main/webapp/jquery/miniBlocks.js
@@ -1,25 +1,44 @@
 $(document).ready(function() {
-    $(".blocks").hover(
-        function(){  
-            $(this).children().css("visibility","visible");        
-        },
-        function(){
-            $(this).children().css("visibility","hidden");
-        }
-    );
+
+    $("body").on("mouseenter",".blocks",function(){
+        $(this).children().css("visibility","visible");
+    });
     
-    $(".miniBlocks").hover(
-        function(){
-            $(this).css("background-color","white");
-            $(this).css("color","black");
-        },
-        function(){
-            $(this).css("background-color","black");
-            $(this).css("color","white");
-        }
-    );
-    $(".miniBlocks").click(function(){
-        console.log("Text: " + $(this).text());
+    $("body").on("mouseleave",".blocks",function(){
+        $(this).children().css("visibility","hidden");
+    });
+    
+    $("body").on("click",".changedBlock",function(){
+        $(this).removeClass("changedBlock").addClass("blocks");
+        $(this).html("<div class= 'miniBlocks'>1</div>"+
+                    "<div class= 'miniBlocks'>2</div>"+
+                    "<div class= 'miniBlocks'>3</div>"+
+                    "<br>"+
+                    "<div class= 'miniBlocks'>4</div>"+
+                    "<div class= 'miniBlocks'>5</div>"+
+                    "<div class= 'miniBlocks'>6</div>"+
+                    "<br>"+
+                    "<div class= 'miniBlocks'>7</div>"+
+                    "<div class= 'miniBlocks'>8</div>"+
+                    "<div class= 'miniBlocks'>9</div>"); 
+        $(this).children().css("visibility","visible");
+    });
+        
+
+    
+    
+    $("body").on("mouseenter",".miniBlocks",function(){
+        $(this).css("background-color","black");
+        $(this).css("color","white");
+    });
+    
+    $("body").on("mouseleave",".miniBlocks",function(){
+        $(this).css("background-color","");
+        $(this).css("color","black");
+    });
+    
+    $("body").on("click",".miniBlocks",function(){
+        $(this).parent().removeClass("blocks").addClass("changedBlock");
         $(this).parent().html($(this).text());
     });
 })
